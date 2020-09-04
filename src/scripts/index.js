d491dcaa3af7c4b1069839af1e7ed87df94ee6c4
Diego Diaz <berith.py@gmail.com>
2020-09-03 23:46:27 -0400

    Basic idea mocked up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
-import { TransformNode } from "@babylonjs/core/Meshes/transformNode";
 import { Vector3 } from "@babylonjs/core/Maths/math";
 import { Color3 } from "@babylonjs/core/Maths/math";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
@@ -12,133 +11,225 @@ import { HighlightLayer } from "@babylonjs/core/Layers/highlightLayer";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
-import "@babylonjs/loaders/glTF";
 import { GridMaterial } from "@babylonjs/materials/grid";
 
 import { PointerEventTypes } from "@babylonjs/core/Events/pointerEvents";
-import { GLTFFileLoader } from "@babylonjs/loaders/glTF/glTFFileLoader";
 import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import { ShadowGenerator } from "@babylonjs/core/Lights/Shadows/shadowGenerator";
 
+import { EasingFunction, CubicEase } from "@babylonjs/core/Animations/easing";
+import { Animation } from "@babylonjs/core/Animations";
 // Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
 import "@babylonjs/core/Meshes/meshBuilder";
+import "@babylonjs/loaders/glTF";
 import "@babylonjs/core";
+
 // Get the canvas element from the DOM.
 const canvas = document.getElementById("renderCanvas");
 
 // Associate a Babylon Engine to it.
 const engine = new Engine(canvas, true, { stencil: true });
+console.log(engine);
 
 // Create our first scene.
 var scene = new Scene(engine);
+scene.clearColor = new Color3(0.3, 0.5, 0.8);
 
 // This creates and positions the camera
+/**
+ * Alpha:8.862160309140117
+   beta:1.2042576861967587
+   radius:5.226759349093842
+   GP:{X: -3.9250482511266886 Y:2.332834600993618 Z:2.4499805763462303} */
+const defaultCameraPosition = new Vector3(
+  -3.9250482511266886,
+  2.332834600993618,
+  2.4499805763462303
+);
 var camera = new ArcRotateCamera(
   "camera1",
-  0,
-  0,
-  0,
-  new Vector3(0, 5, -10),
+  8.862160309140117,
+  1.2042576861967587,
+  2.226759349093842,
+  defaultCameraPosition,
   scene
 );
-// var camera = new UniversalCamera(
-//   "UniversalCamera",
-//   new Vector3(0, 0, -10),
-//   scene
-// );
-// This targets the camera to scene origin
-camera.setTarget(Vector3.Zero());
 
+// This targets the camera to scene origin
+camera.setTarget(new Vector3(0, 0, 0));
 // This attaches the camera to the canvas
 camera.attachControl(canvas, true);
-
+// camera.inputs.addGamepad();
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new HemisphericLight("light1", new Vector3(0, 3, 0), scene);
-var light2 = new HemisphericLight("light1", new Vector3(0, 5, 0), scene);
+const light = new HemisphericLight("light1", new Vector3(0, 3, 0), scene);
+const light2 = new HemisphericLight("light2", new Vector3(0, 5, 0), scene);
 
 // Default intensity is 1. Let's dim the light a small amount
 light.intensity = 1;
 
-// Create a grid material
-// var material = new GridMaterial("grid", scene);
+var anim = "empty";
 
-// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-// var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
+const animEnded = function (anim) {
+  // anim.stop();
+  console.log("animation ended", anim);
+  camera.attachControl(canvas, true);
+  for (anim of scene.animations) {
+    anim.stop();
+  }
+};
 
-// Move the sphere upward 1/2 its height
-// sphere.position.y = 2;
+const targAnimEnded = function (test) {
+  console.log("targAnimEnded:", test);
+};
 
-// Affect a material
-// sphere.material = material;
-// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-// var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
+const camPosAnimEnded = function (test) {
+  console.log("camPosAnimEnded:", test);
+};
 
-// Affect a material
-// ground.material = material;
+const animateCameraTargetToPosition = function (
+  cam,
+  speed,
+  frameCount,
+  newPos
+) {
+  let ease = new CubicEase();
+  ease.setEasingMode(EasingFunction.EASINGMODE_EASEINOUT);
 
-// var myBox = MeshBuilder.CreateBox(
-//   "myBox",
-//   { height: 0.1, width: 2, depth: 0.5 },
-//   scene
-// );
+  let aable1 = Animation.CreateAndStartAnimation(
+    "at5",
+    cam,
+    "target",
+    speed,
+    frameCount,
+    cam.target,
+    newPos,
+    0,
+    ease,
+    targAnimEnded
+  );
+  aable1.disposeOnEnd = true;
+};
 
-// var body = MeshBuilder.CreateCylinder(
-//   "body",
-//   {
-//     height: 0.75,
-//     diameterTop: 0.2,
-//     diameterBottom: 0.5,
-//     tessellation: 6,
-//     subdivisions: 1,
-//   },
-//   scene
-// );
-// var arm = MeshBuilder.CreateBox(
-//   "arm",
-//   { height: 0.75, width: 0.3, depth: 0.1875 },
-//   scene
-// );
-// arm.position.x = 0.125;
-// var pilot = Mesh.MergeMeshes([body, arm], true);
-// let [alpha, beta, gamma] = [0, 0, 1];
-// pilot.rotation.x = alpha;
+const animateCameraToPosition = function (cam, speed, frameCount, position) {
+  let ease = new CubicEase();
+  ease.setEasingMode(EasingFunction.EASINGMODE_EASEINOUT);
+  let aable2 = Animation.CreateAndStartAnimation(
+    "objectAnimation",
+    cam,
+    "position",
+    speed,
+    frameCount,
+    cam.position,
+    position,
+    0,
+    ease,
+    camPosAnimEnded
+  );
+  aable2.disposeOnEnd = true;
+};
+
+const animSpeed = 45;
+const animFrameCount = 200;
 
 SceneLoader.Append("./public/assets/models/", "office.glb", scene, function (
   scene
-) {
-  // Create a default arc rotate camera and light.
-  // scene.createDefaultCameraOrLight(true, true, true);
-  // The default camera looks at the back of the asset.
-  // Rotate the camera by 180 degrees to the front of the asset.
-  scene.activeCamera.alpha += Math.PI;
-});
+) {});
 
+const animateCameraHome = function () {
+  animateCameraToPosition(
+    camera,
+    animSpeed,
+    animFrameCount,
+    defaultCameraPosition
+  );
+  animateCameraTargetToPosition(
+    camera,
+    animSpeed,
+    animFrameCount,
+    new Vector3(0, 0, 0)
+  );
+};
+const animateToMonitor = function () {
+  const monitorPos = new Vector3(
+    -0.6273243739201747,
+    1.5740085977648293,
+    0.5156315816511976
+  );
+  animateCameraToPosition(camera, animSpeed, animFrameCount, monitorPos);
+  const cameraTarget = new Vector3(
+    1.1691228052256966,
+    0.3696672344653039,
+    -0.37055848692617843
+  );
+  animateCameraTargetToPosition(
+    camera,
+    animSpeed * 2,
+    animFrameCount,
+    cameraTarget
+  );
+};
 var highlightLayer = new HighlightLayer("highlightLayer", scene);
+const infoElement = document.getElementById("infoParagraph1");
+const debugElement = document.getElementById("debug");
+const cameraPositionDebugElement = document.getElementById("debugCamera");
 
+//This needs a LOT of refactor
+const whitelist = ["MonitorScreen_primitive1", "MonitorScreen_primitive0"];
 scene.onPointerObservable.add((pointerInfo) => {
   switch (pointerInfo.type) {
-    case PointerEventTypes.POINTERDOWN:
-      break;
     case PointerEventTypes.POINTERMOVE:
+      cameraPositionDebugElement.innerText = `Alpha:${camera.alpha} \n beta:${camera.beta}\n radius:${camera.radius}\n GP:${camera.globalPosition}`;
       break;
     case PointerEventTypes.POINTERPICK:
       const pickedMesh = pointerInfo.pickInfo.pickedMesh;
-      if (pickedMesh.parent && pickedMesh.parent.metadata != null) {
-        pickedMesh.parent.getChildMeshes().forEach((mesh) => {
-          if (highlightLayer.hasMesh(mesh)) {
-            highlightLayer.removeMesh(mesh);
-          } else {
-            highlightLayer.addMesh(mesh, Color3.White());
-          }
-        });
-      } else {
-        // hl is the highlight layer
-        // This checks if the mesh is already in the highlight layer
-        if (highlightLayer.hasMesh(pickedMesh)) {
-          highlightLayer.removeMesh(pickedMesh);
+
+      //This should only happen for whitelisted elements
+      if (whitelist.includes(pickedMesh.name)) {
+        const objectCamera = new Vector3();
+        objectCamera.copyFrom(pickedMesh.position);
+        debugElement.innerText = `pickedMesh position ${
+          pickedMesh.position
+        } objectCamera position ${
+          (objectCamera.x, objectCamera.y, objectCamera.z)
+        }`;
+        /**
+       *  Alpha:2.7085872007963125
+          beta:1.0731817239502128
+          radius:1.4379025790837194
+          GP:{X: -0.6273243739201747 Y:1.5740085977648293 Z:0.5156315816511976}
+       */
+
+        // Most meshes have parents
+        // Having metadata it means that the parent is a TransformNode
+        // otherwise the the parent is a mesh which makes everything
+        // highlighted
+        // checks if the mesh is already in the highlight layer, if it is
+        // it removes the highlight otherwise, it sets the highlight
+        if (pickedMesh.parent && pickedMesh.parent.metadata != null) {
+          pickedMesh.parent.getChildMeshes().forEach((mesh) => {
+            if (highlightLayer.hasMesh(mesh)) {
+              highlightLayer.removeMesh(mesh);
+              animateCameraHome();
+            } else {
+              highlightLayer.addMesh(mesh, Color3.White());
+              infoElement.innerText = mesh.name;
+              if (
+                pickedMesh.name === "MonitorScreen_primitive1" ||
+                pickedMesh.name === "MonitorScreen_primitive0"
+              ) {
+                animateToMonitor();
+              }
+            }
+          });
         } else {
-          highlightLayer.addMesh(pickedMesh, Color3.White());
+          if (highlightLayer.hasMesh(pickedMesh)) {
+            highlightLayer.removeMesh(pickedMesh);
+          } else {
+            highlightLayer.addMesh(pickedMesh, Color3.White());
+          }
         }
       }
+
       break;
   }
 });
